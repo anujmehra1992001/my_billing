9c05192eaa168e9a9e7eda066fdcd4ad5a630f3f
Anuj Mehra <anujmehra181248@gmail.com>
2026-01-29 10:46:47 +0530

    all changes

--- src/app/features/dashboard/pages/notifications/notifications/notifications.ts
+++ src/app/features/dashboard/pages/notifications/notifications/notifications.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notifications',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './notifications.html',
   styleUrl: './notifications.css',
 })
--- src/app/features/dashboard/pages/overview/overview/overview.ts
+++ src/app/features/dashboard/pages/overview/overview/overview.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Signal } from '@angular/core';
 
 @Component({
   selector: 'app-overview',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './overview.html',
   styleUrl: './overview.css',
 })
 export class Overview {
+  loading: boolean = false;
+  summaryCards: any[] = [];
+  recentActivities: any[] = [];
 
+  
+  ngOnInit(): void {
+    this.loadOverviewData();
+  }
+
+  loadOverviewData() {
+    
+    setTimeout(() => {
+      this.summaryCards = [
+        { title: 'Users', value: 1200, icon: 'fa-users', color: 'primary' },
+        { title: 'Orders', value: 320, icon: 'fa-shopping-cart', color: 'success' },
+        { title: 'Revenue', value: 45000, icon: 'fa-rupee-sign', color: 'warning' },
+        { title: 'Pending', value: 18, icon: 'fa-clock', color: 'danger' }
+      ];
+
+      this.recentActivities = [
+        { message: 'New user registered', time: '2 mins ago' },
+        { message: 'Order #234 created', time: '10 mins ago' },
+        { message: 'Payment received', time: '1 hour ago' }
+      ];
+
+      this.loading = false;
+    }, 1000);
+  }
 }
+ 
+
+
+
+
